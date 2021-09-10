77c896e845958ed30313aba8181469a2ef152853
solidlikeplato <ethanwhaley@gmail.com>
2021-09-10 12:19:03 -0500

    fixed a bug

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,7 @@
 class TodoItemsController < ApplicationController
-  before_action :set_author, :set_todo_item
+  before_action :set_author
+  before_action :set_todo_item, except: [:create]
+
   def index
     @todo_items = TodoItem.all
   end
@@ -12,14 +14,19 @@ class TodoItemsController < ApplicationController
     redirect_to @author
   end
 
+  def complete
+    @todo_item.update_attribute(:is_done, !@todo_item.is_done)
+    redirect_to @author
+  end
+
   private
 
   def set_author
-    @author = Author.find(params[:id])
+    @author = Author.find(params[:author_id])
   end
 
   def set_todo_item
-    @todo_item = TodoItem.find(params[:id])
+    @todo_item = @author.todo_items.find(params[:id])
   end
 
   def todo_item_params
